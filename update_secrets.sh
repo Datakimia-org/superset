8ae048db5e16b1d9ecbfa88e6b3250cbd72c6946
root <root@inst-builder-debian-12-build-build-7w84n.us-central1-a.c.gce-image-builder.internal>
2025-01-08 13:22:39 +0000

    add sh script to update superset secret key

---
+++ update_secrets.sh
@@ -0,0 +1,6 @@
+    #!/bin/bash
+    export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id="${INFISICAL_CLIENT_ID}" --client-secret="${INFISICAL_CLIENT_SECRET}" --silent --plain)
+    export INFISICAL_VARIABLES=$(infisical export --format=dotenv --projectId="${INFISICAL_PROJECT_ID}" --env=prod --path="${DESTINY_INFISICAL_PATH}")
+    export SUPERSET_SECRET_KEY=$(echo "${INFISICAL_VARIABLES}" | awk -F"SUPERSET_SECRET_KEY='" '{print $2}' | awk -F"'" '{if ($1 != "") print $1}')
+    sed -i "s/^SUPERSET_SECRET_KEY=.*/SUPERSET_SECRET_KEY='${SUPERSET_SECRET_KEY}'/" "${DESTINY_DIRECTORY_PATH}/superset/docker/.env"
+    sed -i "s/^SUPERSET_JWT_SECRET_KEY=.*/SUPERSET_JWT_SECRET_KEY='${SUPERSET_SECRET_KEY}'/" ${DESTINY_DIRECTORY_PATH}/product-portal-be/.env
